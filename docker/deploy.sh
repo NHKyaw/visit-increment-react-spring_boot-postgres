ff4c33ad30546d3cd115829a66d70bb2207ce7fe
myathincho <myathincho884@gmail.com>
2026-01-26 21:11:18 +0700

    No sudo test

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,8 +1,8 @@
 !#/bin/bash
 set -e      
 
-sudo docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
-sudo docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
+docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
+docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
 
-sudo docker compose down
-sudo docker compose --env-file .env.${IMAGE_TAG} -f docker-compose.yaml up -d
+docker compose down
+docker compose --env-file .env.${IMAGE_TAG} -f docker-compose.yaml up -d
