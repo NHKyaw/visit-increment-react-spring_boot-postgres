54929dc326f9507df0aecbe4b37b7a4807e47702
myathincho <myathincho884@gmail.com>
2026-01-28 00:44:36 +0700

    Testing #11

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,13 +2,12 @@
 set -e
 
 echo "Removing Old Containers and Deploying New Ones with IMAGE_TAG=${IMAGE_TAG}"
-/usr/bin/docker compose down
-/usr/bin/docker rmi nhkyaw/visit-record-app:backend-${IMAGE_TAG} || echo "No old backend image to remove"
-/usr/bin/docker rmi nhkyaw/visit-record-app:frontend-${IMAGE_TAG} || echo "No old frontend image to remove"
+sudo /usr/bin/docker compose down
+sudo /usr/bin/docker rmi nhkyaw/visit-record-app:backend-${IMAGE_TAG} || echo "No old backend image to remove"
+sudo /usr/bin/docker rmi nhkyaw/visit-record-app:frontend-${IMAGE_TAG} || echo "No old frontend image to remove"
 
 
-/usr/bin/docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
-/usr/bin/docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
+sudo /usr/bin/docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
+sudo /usr/bin/docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
 
-
-/usr/bin/docker compose --env-file .env.${IMAGE_TAG} -f docker-compose.yaml up -d
+sudo /usr/bin/docker compose --env-file .env.${IMAGE_TAG} -f docker-compose.yaml up -d
