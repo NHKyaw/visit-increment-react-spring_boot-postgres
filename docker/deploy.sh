26fc5296e2014dd3993c4caca64810bc653e1d1f
myathincho <myathincho884@gmail.com>
2026-01-27 20:50:47 +0700

    Confirm Main Prod

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -17,8 +17,14 @@ set -e
 # /usr/bin/docker --version
 # /usr/bin/docker compose version
 
+echo "Removing Old Containers and Deploying New Ones with IMAGE_TAG=${IMAGE_TAG}"
+/usr/bin/docker compose down
+/usr/bin/docker rmi nhkyaw/visit-record-app:backend-${IMAGE_TAG} || echo "No old backend image to remove"
+/usr/bin/docker rmi nhkyaw/visit-record-app:frontend-${IMAGE_TAG} || echo "No old frontend image to remove"
+
+
 /usr/bin/docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
 /usr/bin/docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
 
-/usr/bin/docker compose down
+
 /usr/bin/docker compose --env-file .env.${IMAGE_TAG} -f docker-compose.yaml up -d
