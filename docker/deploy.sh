0c5a6c9fccd5ff5b49978270a7d4c81f4ace7b36
myathincho <myathincho884@gmail.com>
2026-01-27 20:08:59 +0700

    Change Deployment Style#06

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
 
-cd ~/docker/
-id >> ~/docker/data.log
-whoami >> ~/docker/data.log
-groups >> ~/docker/data.log
-printenv | grep -i IMAGE_TAG || (echo "IMAGE_TAG is not set") >> ~/docker/data.log
+# cd ~/docker/
+# id >> ~/docker/data.log
+# whoami >> ~/docker/data.log
+# groups >> ~/docker/data.log
+# printenv | grep -i IMAGE_TAG || (echo "IMAGE_TAG is not set") >> ~/docker/data.log
 
-docker --version >> ~/docker/docker.log
-sudo docker --version > ~/docker/docker.log
-/user/bin/docker --version >> ~/docker/docker.log
-sudo /usr/bin/docker --version >> ~/docker/docker.log
+# docker --version >> ~/docker/docker.log
+# sudo docker --version >> ~/docker/docker.log
+# /user/bin/docker --version >> ~/docker/docker.log
+# sudo /usr/bin/docker --version >> ~/docker/docker.log
 
-sudo usermod -aG docker $USER || echo "Failed to add $USER to docker group"
-echo "Using docker at: $(which docker)"
-/usr/bin/docker --version
-/usr/bin/docker compose version
+# sudo usermod -aG docker $USER || echo "Failed to add $USER to docker group"
+# echo "Using docker at: $(which docker)"
+# /usr/bin/docker --version
+# /usr/bin/docker compose version
 
 /usr/bin/docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
 /usr/bin/docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
