4dea8682b18389c40d11a1cd29c4a92622cb201b
myathincho <myathincho884@gmail.com>
2026-01-27 17:36:10 +0700

    Testing with Appleboy Script_Path

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -7,12 +7,12 @@ printenv | grep -i IMAGE_TAG || (echo "IMAGE_TAG is not set")
 
 sudo usermod -aG docker $USER || echo "Failed to add $USER to docker group"
 echo "Using docker at: $(which docker)"
-usr/bin/docker --version
-usr/bin/docker --version
-usr/bin/docker compose version
+/usr/bin/docker --version
+/usr/bin/docker --version
+/usr/bin/docker compose version
 
-usr/bin/docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
-usr/bin/docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
+/usr/bin/docker pull nhkyaw/visit-record-app:backend-${IMAGE_TAG}
+/usr/bin/docker pull nhkyaw/visit-record-app:frontend-${IMAGE_TAG}
 
-usr/bin/docker compose down
-usr/bin/docker compose --env-file .env.${IMAGE_TAG} -f docker-compose.yaml up -d
+/usr/bin/docker compose down
+/usr/bin/docker compose --env-file .env.${IMAGE_TAG} -f docker-compose.yaml up -d
