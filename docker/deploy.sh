89c28db3ebb72e43726160ab8a29054f19a4d976
myathincho <myathincho884@gmail.com>
2026-01-28 00:48:17 +0700

    Testing #12

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Removing Old Containers and Deploying New Ones with IMAGE_TAG=${IMAGE_TAG}"
+cd /home/ubuntu/docker/
 sudo /usr/bin/docker compose down
 sudo /usr/bin/docker rmi nhkyaw/visit-record-app:backend-${IMAGE_TAG} || echo "No old backend image to remove"
 sudo /usr/bin/docker rmi nhkyaw/visit-record-app:frontend-${IMAGE_TAG} || echo "No old frontend image to remove"
