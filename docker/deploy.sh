fda77b25e576bf7f70cdd759329c5b2de9a1c6ed
myathincho <myathincho884@gmail.com>
2026-01-27 23:40:15 +0700

    Deploy Pipeline Fix

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 set -e
 
-# cd ~/docker/
-# id >> ~/docker/data.log
-# whoami >> ~/docker/data.log
-# groups >> ~/docker/data.log
-# printenv | grep -i IMAGE_TAG || (echo "IMAGE_TAG is not set") >> ~/docker/data.log
-
-# docker --version >> ~/docker/docker.log
-# sudo docker --version >> ~/docker/docker.log
-# /user/bin/docker --version >> ~/docker/docker.log
-# sudo /usr/bin/docker --version >> ~/docker/docker.log
-
-# sudo usermod -aG docker $USER || echo "Failed to add $USER to docker group"
-# echo "Using docker at: $(which docker)"
-# /usr/bin/docker --version
-# /usr/bin/docker compose version
-
 echo "Removing Old Containers and Deploying New Ones with IMAGE_TAG=${IMAGE_TAG}"
 /usr/bin/docker compose down
 /usr/bin/docker rmi nhkyaw/visit-record-app:backend-${IMAGE_TAG} || echo "No old backend image to remove"
