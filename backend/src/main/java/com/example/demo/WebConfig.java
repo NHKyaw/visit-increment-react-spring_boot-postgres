93495ac9937aa1bbf49acce516a0642b6d8ac2d6
Nyan Htet Kyaw <nhk.stth@gmail.com>
2026-04-24 11:18:26 +0700

    Fix: BE test error#01

--- backend/src/main/java/com/example/demo/WebConfig.java
+++ backend/src/main/java/com/example/demo/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public final class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     /**
      * Adds CORS mappings.
